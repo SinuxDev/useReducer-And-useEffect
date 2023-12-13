9e82661e2082e5af31f4f48e7d2a6c18cea0b772
Sinux <aung.atg1738@gmail.com>
2023-12-13 21:35:32 +0630

    FirstOneData

--- src/App.jsx
+++ src/App.jsx
@@ -3,40 +3,9 @@ import { useEffect, useState } from "react";
 function App() {
     const [todos,setTodos] = useState([]);
 
-    useEffect( () => {
-        fetchData();
-    },[]);
-
-    const fetchData = async () =>{
-      const response = await fetch('https://jsonplaceholder.typicode.com/todos/');
-      const data = await response.json();
-      setTodos(data);
-    }
-
     return (
       <section>
-          <table>
-            <thead>
-              <tr>
-                <th>id</th>
-                <th>title</th>
-                <th>completed</th>
-              </tr>
-            </thead>
-            <tbody>
-              {
-                todos.map((todo) =>(
-                  <tr key={todo.id} >
-                    <td> {todo.id} </td>
-                    <td> {todo.title} </td>
-                    {
-                      todo.completed ? (<td>Done</td>) : (<td>None</td>)
-                    }
-                  </tr>
-                ))
-              }
-            </tbody>
-          </table>
+          <input type="text" />
       </section>
     );
 
