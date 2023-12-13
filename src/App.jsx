65c56b105467c10f996282379eb9664f7610986d
Sinux <aung.atg1738@gmail.com>
2023-12-13 19:27:15 +0630

    Fetch Function

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
 import { useEffect, useState } from "react";
 
 function App() {
-    
+    useEffect( () => {
+        async function fetchData(){
+          const response = await fetch('https://jsonplaceholder.typicode.com/todos/');
+          const data = await response.json();
+          console.log(data);
+        }
+        fetchData();
+    },[]);
+
+    return (
+      <>
+        <h1>Hello React</h1>
+      </>
+    );
+
+
+
 }
 
 export default App;
