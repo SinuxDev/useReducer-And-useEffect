6d360c34738da17e1163cd1c734de911d94f1259
Sinux <aung.atg1738@gmail.com>
2023-12-13 20:11:45 +0630

    CreateTable

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,41 @@
 import { useEffect, useState } from "react";
 
 function App() {
+    const [todos,setTodos] = useState([]);
+
     useEffect( () => {
-        async function fetchData(){
-          const response = await fetch('https://jsonplaceholder.typicode.com/todos/');
-          const data = await response.json();
-          console.log(data);
-        }
         fetchData();
     },[]);
 
+    const fetchData = async () =>{
+      const response = await fetch('https://jsonplaceholder.typicode.com/todos/');
+      const data = await response.json();
+      setTodos(data);
+    }
+
     return (
-      <>
-        <h1>Hello React</h1>
-      </>
+      <section>
+          <table>
+            <thead>
+              <tr>
+                <th>id</th>
+                <th>title</th>
+                <th>completed</th>
+              </tr>
+            </thead>
+            <tbody>
+              {
+                todos.map((todo) =>(
+                  <tr key={todo.id} >
+                    <td> {todo.id} </td>
+                    <td> {todo.title} </td>
+                    <td> {todo.completed} </td>
+                  </tr>
+                ))
+              }
+            </tbody>
+          </table>
+      </section>
     );
 
 
