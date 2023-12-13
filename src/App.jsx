cbe54c2a05cbf927c271600c0d983de70376e277
Sinux <aung.atg1738@gmail.com>
2023-12-13 19:02:02 +0630

    FirstStepApi

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,7 @@
 import { useEffect, useState } from "react";
 
 function App() {
-  const [key,setKey] = useState("");
-  useEffect(()=>{
-    console.log("I'm useEffect Hooks");
-  },[])
-
-  return (
-    <div>
-      <h1>Hello React</h1>
-      <input type="text" onChange={(e)=>{
-        setKey(e.target.value);
-      }} />
-    </div>
-  );
+    
 }
 
 export default App;
