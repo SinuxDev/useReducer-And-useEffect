f9c84b7206c40e51c263319e9757be6d1acd85cb
Sinux <aung.atg1738@gmail.com>
2023-12-13 21:56:22 +0630

    SearchComplete

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 function App() {
-    const [todos,setTodos] = useState([]);
+    const [id,setID] = useState("");
+
+    const getData = async (e) =>{
+      e.preventDefault();
+      const response = await fetch(`https://jsonplaceholder.typicode.com/todos/${id}`);
+      const data = await response.json();
+      console.log(data);
+    }
 
     return (
       <section>
-          <input type="text" />
+          <form action="" onSubmit={getData} >
+            <input type="text" value={id} onChange={(e)=>{
+              setID(e.target.value);
+            }} />
+            <button type="submit">Get Data</button>
+          </form>
       </section>
     );
 
