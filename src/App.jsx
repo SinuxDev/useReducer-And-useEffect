ad69bbda24841fd1bce37cc4ac5f0ebaf0f0a365
Sinux <aung.atg1738@gmail.com>
2023-12-13 22:35:29 +0630

    Completed

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,22 @@ import { useState } from "react";
 
 function App() {
     const [id,setID] = useState("");
+    const [error,setError] = useState(false);
+    const [toDo,setTodo] = useState(undefined);
 
     const getData = async (e) =>{
       e.preventDefault();
+      if( id < 1 ){
+        setError(true);
+        setID("");
+        return;
+      }
       const response = await fetch(`https://jsonplaceholder.typicode.com/todos/${id}`);
       const data = await response.json();
-      console.log(data);
+      setError(false);
+      setTodo(data);
+
+      setID("");
     }
 
     return (
@@ -18,6 +28,27 @@ function App() {
             }} />
             <button type="submit">Get Data</button>
           </form>
+         <div>
+            {
+              error && <h1>Pleas Enter Valid ID Number Eg. 1, 2, 3 </h1>
+            }
+
+            {
+              toDo && (<di>
+                <h1> User ID : {toDo.userId} </h1>
+                <h1> User Title :  {toDo.title} </h1>
+                <h1> ID :  {toDo.id} </h1>
+                <h1> 
+                  Completed - {""}
+                  {
+                  toDo.completed ? (<span>Done</span>) : (<span>None</span>)
+                  } </h1>
+              </di>)
+            }
+         </div>
+
+
+
       </section>
     );
 
