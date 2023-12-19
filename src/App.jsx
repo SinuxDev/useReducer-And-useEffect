f0fa3d8ca132e8582515c8a1643fcc7d0a2c827e
Sinux <aung.atg1738@gmail.com>
2023-12-19 23:10:50 +0630

    CustomFirst

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,11 @@
 import { useEffect, useState } from "react";
 
 function App() {
-  const [key,setKey] = useState("");
-  useEffect(()=>{
-    console.log("I'm useEffect Hooks");
-  },[])
-
+  
   return (
-    <div>
-      <h1>Hello React {key} </h1>
-      <input type="text" onChange={(e)=>{
-        setKey(e.target.value);
-      }} />
-    </div>
+    <>
+     <h1>Hello World</h1> 
+    </>
   );
 }
 
