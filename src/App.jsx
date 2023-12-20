6b4010e18186b7c12f229f28c3ed7d197b9d2786
Sinux <aung.atg1738@gmail.com>
2023-12-20 12:57:03 +0630

    ApiFun

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,19 @@
 import { useEffect, useState } from "react";
 
 function App() {
+
+  const [products,setProducts] = useState([]);
+
+  useEffect(() => {
+
+  },[])
+
+  const getProducts = async () => {
+    const response = await fetch('https://fakestoreapi.com/products/');
+    const data = await response.json();
+    return data;
+  }
+
   return (
     <section>
       <h1>Hello React</h1>
